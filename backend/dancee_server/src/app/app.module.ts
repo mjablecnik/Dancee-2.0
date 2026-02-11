f1d71f870cefc344a695d55e30e8e55a92461539
Martin Jablečník <martin.jablecnik@email.cz>
2026-02-11 14:26:11 +0100

    refactor: move app files to app directory

--- backend/dancee_server/src/app/app.module.ts
+++ backend/dancee_server/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ScraperModule } from './scraper/scraper.module';
-import { EventsModule } from './events/events.module';
-import { FirebaseModule } from './firebase/firebase.module';
+import { ScraperModule } from '../scraper/scraper.module';
+import { EventsModule } from '../events/events.module';
+import { FirebaseModule } from '../firebase/firebase.module';
 
 @Module({
   imports: [FirebaseModule, ScraperModule, EventsModule],
--- backend/dancee_server/src/main.ts
+++ backend/dancee_server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 const basicAuth = require('express-basic-auth');
 
 async function bootstrap() {
